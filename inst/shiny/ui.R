a8c1c230a0b4f4a6098a3d0b300fb86bf74ec1a1
dkneis <david.kneis@tu-dresden.de>
2015-07-17 12:10:07 +0000

    ENH: Use of tab panels

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,4 +1,3 @@
-
 ################################################################################
 # Method to generate GUI code
 
@@ -7,112 +6,147 @@ ui_generate= function(vars, pars) {
   labStyle= "font-family: Arial Narrow; line-height: 10px; color: #3E566A"
   headStyle= "font-weight: bold; color: #3E566A"
   newline="\n"
-  code="# THIS IS A GENERATED FILE -- DO NOT EDIT"
-  code=paste0(code,newline,"",newline)
-  code=paste0(code,"shinyUI(",newline)
-  code=paste0(code,"  fluidPage(",newline)
-  code=paste0(code,"    fluidRow(",newline)
-  code=paste0(code,"      column(2,",newline)
-  code=paste0(code,"        p(style='",headStyle,"', 'Parameters')",newline)
-  code=paste0(code,"      ),",newline)
-  code=paste0(code,"      column(2,",newline)
-  code=paste0(code,"        p(style='",headStyle,"', 'Variables'),",newline)
-  code=paste0(code,"        fluidRow(",newline)
-  code=paste0(code,"          column(5,",newline)
-  code=paste0(code,"            p(style='",headStyle,"', 'Init.')",newline)
-  code=paste0(code,"          ),",newline)
-  code=paste0(code,"          column(4,",newline)
-  code=paste0(code,"            p(style='",headStyle,"', 'Mult.')",newline)
-  code=paste0(code,"          ),",newline)
-  code=paste0(code,"          column(3,",newline)
-  code=paste0(code,"            p(style='",headStyle,"', 'On')",newline)
-  code=paste0(code,"          )",newline)
-  code=paste0(code,"        )",newline)
-  code=paste0(code,"      )",newline)
-  code=paste0(code,"    ),",newline)
-  code=paste0(code,"    fluidRow(",newline)
-  # Parameters
-  rows= which(as.logical(pars$user))
-  code=paste0(code,"      column(2,style = 'overflow-y:scroll; max-height: 800px',",newline)
-  if (length(rows) > 0) {
-    for (i in rows) {
-      code=paste0(code,"      div(style='",labStyle,"',textInput('",pars$name[i],"', label = '",
-        pars$label[i],"', value=",pars$default[i],"))",
-        ifelse(i==rows[length(rows)],"",","),newline)
+
+  # Dynamic code: Text boxes for parameters
+  inputPars= function() {
+    code=""
+    rows= which(as.logical(pars$user))
+    if (length(rows) > 0) {
+      for (i in rows) {
+        code=paste0(code,"      div(style='",labStyle,"',textInput('",pars$name[i],"', label = '",
+          pars$label[i],"', value=",pars$default[i],"))",
+          ifelse(i==rows[length(rows)],"",","),newline)
+      }
+    } else {
+      code=paste0(code,"p('No visible items')",newline)
     }
-  } else {
-    code=paste0(code,"p('No visible items')",newline)
+    return(code)
   }
-  code=paste0(code,"      ),",newline)
-  # Variables
-  code=paste0(code,"      column(2,style = 'overflow-y:scroll; max-height: 800px',",newline)
-  for (i in 1:nrow(vars)) {
-    code=paste0(code,"        fluidRow(",newline)
-    code=paste0(code,"          column(5,",newline)
-    code=paste0(code,"            div(style='",labStyle,"',textInput('",vars$name[i],
-      "', label = '",vars$label[i],"', value=",vars$default[i],"))",newline)
-    code=paste0(code,"          ),",newline)
-    code=paste0(code,"          column(4,",newline)
-    code=paste0(code,"            div(style='",labStyle,"',textInput('",vars$name[i],
-      ".mult', label = ' ', value=",vars$mult[i],"))",newline)
-    code=paste0(code,"          ),",newline)
-    code=paste0(code,"          column(3,",newline)
-    code=paste0(code,"            div(style='",labStyle,"',checkboxInput('",vars$name[i],
-      ".show', label = ' ', value=",as.logical(vars$show[i]),"))",newline)
-    code=paste0(code,"          )",newline)
-    code=paste0(code,"        )",ifelse(i==nrow(vars),"",","),newline)
+
+  # Dynamic code: Text boxes for variables
+  inputVars= function() {
+    code=""
+    for (i in 1:nrow(vars)) {
+      code=paste0(code,"        fluidRow(",newline)
+      code=paste0(code,"          column(5,",newline)
+      code=paste0(code,"            div(style='",labStyle,"',textInput('",vars$name[i],
+        "', label = '",vars$label[i],"', value=",vars$default[i],"))",newline)
+      code=paste0(code,"          ),",newline)
+      code=paste0(code,"          column(4,",newline)
+      code=paste0(code,"            div(style='",labStyle,"',textInput('",vars$name[i],
+        ".mult', label = ' ', value=",vars$mult[i],"))",newline)
+      code=paste0(code,"          ),",newline)
+      code=paste0(code,"          column(3,",newline)
+      code=paste0(code,"            div(style='",labStyle,"',checkboxInput('",vars$name[i],
+        ".show', label = ' ', value=",as.logical(vars$show[i]),"))",newline)
+      code=paste0(code,"          )",newline)
+      code=paste0(code,"        )",ifelse(i==nrow(vars),"",","),newline)
+    }
+    return(code)
   }
-  code=paste0(code,"      ),",newline)
 
-  code=paste0(code,"      column(8,",newline)
-  code=paste0(code,"        fluidRow(",newline)
-  code=paste0(code,"          column(12,",newline)
-  code=paste0(code,"            plotOutput(outputId='plotStates', height='600px', width='90%')",newline)
-  code=paste0(code,"          )",newline)
-  code=paste0(code,"        ),",newline)
-  code=paste0(code,"        fluidRow(",newline)
-  code=paste0(code,"          column(2,",newline)
-  code=paste0(code,"            div(style='",labStyle,"',textInput('time.min', label = 'min. time', value=0))",newline)
-  code=paste0(code,"          ),",newline)
-  code=paste0(code,"          column(2,",newline)
-  code=paste0(code,"            div(style='",labStyle,"',textInput('time.max', label = 'max. time', value=10))",newline)
-  code=paste0(code,"          ),",newline)
-  code=paste0(code,"          column(2,",newline)
-  code=paste0(code,"            div(style='",labStyle,"',textInput('time.dt', label = 'time step', value=1))",newline)
-  code=paste0(code,"          )",newline)
-  code=paste0(code,"        ),",newline)
-  code=paste0(code,"        fluidRow(",newline)
-  code=paste0(code,"          column(2,",newline)
-  code=paste0(code,"            div(style='",labStyle,"',textInput('y.min', label = 'min. Y', value=",min(vars$default)*ifelse(min(vars$default) < 0,2,0.5),"))",newline)
-  code=paste0(code,"          ),",newline)
-  code=paste0(code,"          column(2,",newline)
-  code=paste0(code,"            div(style='",labStyle,"',textInput('y.max', label = 'max. Y', value=",max(vars$default)*ifelse(max(vars$default) < 0,0.5,2),"))",newline)
-  code=paste0(code,"          ),",newline)
-  code=paste0(code,"          column(2,",newline)
-  code=paste0(code,"            div(style='",labStyle,"',checkboxInput('logY', label='Log Y', value = FALSE))",newline)
-  code=paste0(code,"          )",newline)
-  code=paste0(code,"        ),",newline)
-  code=paste0(code,"        fluidRow(",newline)
-  code=paste0(code,"          column(2,",newline)
-  code=paste0(code,"            div(style='",labStyle,"',actionButton('setRef', label='Set as ref.'))",newline)
-  code=paste0(code,"          ),",newline)
-  code=paste0(code,"          column(2,",newline)
-  code=paste0(code,"            div(style='",labStyle,"',checkboxInput('showRef', label='Show ref.', value = FALSE))",newline)
-  code=paste0(code,"          )",newline)
-  code=paste0(code,"        ),",newline)
-  code=paste0(code,"        fluidRow(",newline)
-  code=paste0(code,"          column(12,",newline)
-  code=paste0(code,"            p(style='",headStyle,"', 'Stoichiometry matrix'),",newline)
-  code=paste0(code,"            htmlOutput(outputId='visStoi', inline=FALSE)",newline)
-  code=paste0(code,"          )",newline)
-  code=paste0(code,"        )",newline)
-  code=paste0(code,"      )",newline)
-  code=paste0(code,"    )",newline)
-  code=paste0(code,"  )",newline)
-  code=paste0(code,")",newline)
+  # --- BEGIN OF UI CODE ---
+  code= paste0(
+  "
+  # THIS IS A GENERATED FILE -- DO NOT EDIT
+  shinyUI(
+    navbarPage('rodeoApp',
 
+      # New tab panel
+      tabPanel('Dynamic simulation',
+        fluidRow(
+          column(2,
+            p(style='",headStyle,"', 'Parameters')
+          ),
+          column(2,
+            p(style='",headStyle,"', 'Variables'),
+            fluidRow(
+              column(5,
+                p(style='",headStyle,"', 'Init.')
+              ),
+              column(4,
+                p(style='",headStyle,"', 'Mult.')
+              ),
+              column(3,
+                p(style='",headStyle,"', 'On')
+              )
+            )
+          )
+        ),
+        fluidRow(
+          column(2,style = 'overflow-y:scroll; max-height: 800px',
+    ",
+    # Parameters
+    inputPars(),
+    "
+          ),
+          column(2,style = 'overflow-y:scroll; max-height: 800px',
+    ",
+    # Variables
+    inputVars(),
+    "
+          ),
+          column(8,
+            fluidRow(
+              column(12,
+                plotOutput(outputId='plotStates', height='600px', width='90%')
+              )
+            ),
+            fluidRow(
+              column(2,
+                div(style='",labStyle,"',textInput('time.min', label = 'min. time', value=0))
+              ),
+              column(2,
+                div(style='",labStyle,"',textInput('time.max', label = 'max. time', value=10))
+              ),
+              column(2,
+                div(style='",labStyle,"',textInput('time.dt', label = 'time step', value=1))
+              )
+            ),
+            fluidRow(
+              column(2,
+                div(style='",labStyle,"',textInput('y.min', label = 'min. Y',
+                  value=",min(vars$default)*ifelse(min(vars$default) < 0,2,0.5),"))
+              ),
+              column(2,
+                div(style='",labStyle,"',textInput('y.max', label = 'max. Y',
+                   value=",max(vars$default)*ifelse(max(vars$default) < 0,0.5,2),"))
+              ),
+              column(2,
+                div(style='",labStyle,"',checkboxInput('logY', label='Log Y', value = FALSE))
+              )
+            ),
+            fluidRow(
+              column(2,
+                div(style='",labStyle,"',actionButton('setRef', label='Set as ref.'))
+              ),
+              column(2,
+                div(style='",labStyle,"',checkboxInput('showRef', label='Show ref.', value = FALSE))
+              )
+            )
+          )
+        )
+      ), # End tabPanel
+
+      # New tab panel
+      tabPanel('Peterson matrix',
+        fluidRow(
+          column(12,
+            htmlOutput(outputId='visStoi', inline=FALSE)
+          )
+        )
+
+      ) # End tabPanel
+
+    ) # End Navbar page
+  ) # End shinyUI
+  "
+  ) 
+  # --- END OF UI CODE ---
   return(code)
 }
+
+
 
 #code= ui_generate(vars=get("rodeoApp.vars",envir=globalenv()),
 #  pars=get("rodeoApp.pars",envir=globalenv()))
@@ -129,4 +163,4 @@ eval(parse(text=ui_generate(
   vars=get("rodeoApp.vars",envir=globalenv()),
   pars=get("rodeoApp.pars",envir=globalenv())
 )))
-  
+
