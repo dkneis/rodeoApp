6a5e6a3024ad89a5130249d13f2f42f4ff867b85
dkneis <david.kneis@tu-dresden.de>
2015-08-13 14:09:48 +0000

    FIX: Handling of directory separators on Windows

--- R/runable.r
+++ R/runable.r
@@ -29,7 +29,7 @@ runGUI= function(
   tables= c(vars="vars",pars="pars",funs="funs",pros="pros",stoi="stoi"),
   colsep=",", obs=NULL
 ) {
-  dir= normalizePath(dir)
+  dir= normalizePath(dir, winslash="/")
   # Run init
   ini= initModel(dir=dir, xlFile=xlFile, funsR=funsR, funsF=funsF,
     tables=tables, colsep=colsep)
@@ -106,7 +106,7 @@ runMCS= function(
   colsep=",",
   silent=FALSE
 ) {
-  dir= normalizePath(dir)
+  dir= normalizePath(dir, winslash="/")
   # Initialize model
   ini= initModel(dir=dir, xlFile=xlFile, funsR=funsR, funsF=funsF,
     tables=tables, colsep=colsep)
